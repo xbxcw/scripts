7f0292690a5d0c9c637a40dfb73554a9cbbe55c0
Unknown <hanyuncheng@outlook.com>
2018-10-14 02:22:10 +0800

    0.0

--- testcode.py
+++ testcode.py
@@ -1,27 +1,62 @@
 from maya import cmds as mc
+import time
+import threading
 
 def test():
-    uvs = mc.ls(mc.polyListComponentConversion(tuv=True),fl=True)
-    # xPositions = sorted([mc.polyEditUV(i, query=True)[0] for i in uvs])
-    # yPositions = sorted([mc.polyEditUV(i, query=True)[1] for i in uvs])
+    time_start=time.time()
+    edges = mc.ls(mc.polyListComponentConversion(te=True),fl=True)
     a = []
-    c = []
-    b = {}
-    e = {}
-    f = 0
-    for uv in uvs:
-        print uv
-        p = mc.polyEditUV(uv, query=True)
-        if p not in a:
-            b[uv] = 1
+    b = []
+    for edge in edges:
+        mc.select(edge)
+        uvs = mc.ls(mc.polyListComponentConversion(tuv=True),fl=True)
+        for uv in uvs:
+            p = mc.polyEditUV(uv, query=True)
             a.append(p)
-        else:
-            b[uv] = 0
-            b[uv] = 1 + b[uv] 
-
+            print a
+        if a[0] == a[1] and a[2] == a[3]:
+            b.append(edge)
+        elif a[0] == a[2] and a[1] == a[3]:
+            b.append(edge)
+        elif a[0] == a[3] and a[1] == a[2]:
+            b.append(edge)
+        a = []
     print b
-    for (key, value) in b.items():
-        if value > 1:
-            print key, value
+    
 
+
+    mc.select(b)
+    mc.polyMapSewMove()
+    time_end=time.time()
+    print '%.2fs' % (time_end - time_start)
+    # mc.ConvertSelectionToContainedEdges()
+
+def test1():
+    time_start=time.time()
+    edges = mc.ls(mc.polyListComponentConversion(te=True),fl=True)
+    print len(edges)
+    a = []
+    b = []
+    for edge in edges:
+        mc.select(edge)
+        uvs = mc.ls(mc.polyListComponentConversion(tuv=True),fl=True)
+        for uv in uvs:
+            p = mc.polyEditUV(uv, query=True)
+            a.append(p)
+            print a
+
+        if (a[0] == a[1] and a[2] == a[3]) or (a[0] == a[2] and 
+            a[1] == a[3]) or (a[0] == a[3] and a[1] == a[2]):
+            b.append(edge)
+
+        a = []
+    print b
+    
+
+
+    mc.select(b)
+    mc.polyMapSewMove()
+    time_end=time.time()
+    print '%.2fs' % (time_end - time_start)
+    # mc.ConvertSelectionToContainedEdges()
 
